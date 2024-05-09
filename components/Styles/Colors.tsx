cffb66e7b31e8b994a94e3a2437947a26c564ac3
SmilinAtYa <smlnatya@gmail.com>
2024-05-09 10:41:49 -0400

    Colors

--- components/Styles/Colors.tsx
+++ components/Styles/Colors.tsx
@@ -1,1 +1,5 @@
 export const gray = "rgba(51, 51, 51, 1)";
+export const lightGray = "rgba(203, 203, 203, 1)";
+export const lightestGray = "rgba(248, 248, 248, 1))";
+export const blue = "rgba(12, 178, 251, 1)";
+export const white = "rgba(252, 252, 252, 1)";
---
+++ components/Styles/types.ts
@@ -0,0 +1,3 @@
+import * as Colors from "../Styles/Colors";
+
+export type ColorNames = keyof typeof Colors;
