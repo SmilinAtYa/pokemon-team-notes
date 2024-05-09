fef1f1848f350f4100be86a8ad69051fad3be0ae
SmilinAtYa <smlnatya@gmail.com>
2024-05-09 10:40:25 -0400

    Updating icon style

--- components/PokemonIcon.tsx
+++ components/PokemonIcon.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+import { lightGray } from "./Styles/Colors";
+import { ScaledSheet } from "react-native-size-matters";
 
 interface PokemonIconProps {
   name?: string;
@@ -40,14 +42,29 @@ const PokemonIcon = ({ name }: PokemonIconProps) => {
 
   if (!imageLink) return;
 
-  return <Image style={styles.tinyLogo} src={imageLink} />;
+  return (
+    <View style={styles.container}>
+      <Image style={styles.tinyLogo} src={imageLink} />
+    </View>
+  );
 };
 
 export default PokemonIcon;
 
-const styles = StyleSheet.create({
+const styles = ScaledSheet.create({
+  container: {
+    marginTop: "8@s",
+    borderRadius: "53@s",
+    borderWidth: "1.5@s",
+    borderColor: lightGray,
+    padding: "2.5@s",
+    width: "50@s",
+    height: "50@s",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   tinyLogo: {
-    width: 100,
-    height: 100,
+    width: "40@s",
+    height: "40@s",
   },
 });
